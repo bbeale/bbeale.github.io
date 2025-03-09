ae929bfe409db0896634adf26baae1219e0d6252
Ben <beale.ben@gmail.com>
2025-03-09 11:37:22 -0400

    Row / col layout for blog entries.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -40,7 +40,7 @@ xhr.onload = () => {
       postCommentsCount = resp.comments_count;
 
       postNode = document.createElement("div");
-      postNode.className = "blog-card";
+      postNode.className = "blog-card col";
 
       postNodeImage = document.createElement("img");
       postNodeImage.className = "card-img-top";
